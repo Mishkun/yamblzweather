93d5fcbb14227417f70a554a26d4070ff109c373
Vladimir Kondenko <kondenko2011@gmail.com>
2017-07-14 13:17:27 +0300

    Add a slash to the end of the base URL

--- app/src/main/java/com/kondenko/yamblzweather/Const.java
+++ app/src/main/java/com/kondenko/yamblzweather/Const.java
@@ -2,6 +2,6 @@ package com.kondenko.yamblzweather;
 
 public abstract class Const {
 
-    public static String BASE_URL = "http://api.apixu.com/v1";
+    public static String BASE_URL = "http://api.apixu.com/v1/";
 
 }
